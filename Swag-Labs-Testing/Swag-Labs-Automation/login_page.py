9fd056489cf48ce6c15170b5c936f282c17d94b9
Reeti Thapa <reetithapa111@gmail.com>
2026-01-15 15:24:03 +0545

    Update login_page.py

--- Swag-Labs-Testing/Swag-Labs-Automation/login_page.py
+++ Swag-Labs-Testing/Swag-Labs-Automation/login_page.py
@@ -1,1 +1,17 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def login(driver, username = "standard_user", password = "secret_sauce"):
+    driver.get("https://www.saucedemo.com/")
+
+    driver.find_element(By.ID, "user-name").send_keys(username)
+    driver.find_element(By.ID, "password").send_keys(password)
+    driver.find_element(By.ID, "login-button").click()
+
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "inventory_container")))
+
+    assert "inventory.html" in driver.current_url, "Login did not redirect to inventory page"
+    print("Login successful")
+
 
